84ae2b6b9e3c8c97b41c3af3656962adfa959d2a
shamsullah-shams <shamsullah07073@gmail.com>
2023-03-19 11:33:53 +0430

    Models were created

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ClassModel;
 
 class ClassController extends Controller
 {
--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Teacher;
 
 class TeacherController extends Controller
 {
